c7ff4745d153ebf8287a68ebaf3ae902dadd5420
Rickard Brüel Gabrielsson <rbg@Rickards-MacBook-Pro.local>
2024-05-16 10:12:32 -0400

    M

--- lolas.py
+++ lolas.py
@@ -407,14 +407,14 @@ def lola_loras(lora_module_list, cache, r=8, type="diagonal", sparse_reg=0, tran
         if type == "diagonal":
             U, V, sigmas = diagonal_lora_pca_sparse_wrapper(As,Bs,r,niter=10, display=False, sparse_reg=sparse_reg)    
         elif type == "full":
-            #U, V, sigmas = full_lora_pca_wrapper(As,Bs,r,niter=10, display=False) 
+            U, V, sigmas = full_lora_pca_wrapper(As,Bs,r,niter=10, display=False) 
 
-            for i in range(10):
-                device = torch.device("cuda")
-                U, V, sigmas = full_lora_pca_wrapper(As,Bs,r,niter=10, display=False) 
-                reconstruction_error = torch.pow( torch.norm(Bs[i].to(device) @ As[i].to(device) - U @ sigmas[i].to(device) @ V.t(), p='fro') / torch.norm(Bs[i].to(device) @ As[i].to(device), p='fro'), 2)
-                print("reconstruction_error", reconstruction_error)
-            assert(False)
+            # for i in range(10):
+            #     device = torch.device("cuda")
+            #     U, V, sigmas = full_lora_pca_wrapper(As,Bs,r,niter=10, display=False) 
+            #     reconstruction_error = torch.pow( torch.norm(Bs[i].to(device) @ As[i].to(device) - U @ sigmas[i].to(device) @ V.t(), p='fro') / torch.norm(Bs[i].to(device) @ As[i].to(device), p='fro'), 2)
+            #     print("reconstruction_error", reconstruction_error)
+            # assert(False)
         elif type == "SVD":
             Us, Vs, Sigmas = [], [], []
             for i in range(len(As)):
