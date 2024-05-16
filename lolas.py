7be56dbdd2e56b6bba5114607139c289ebf89b66
Rickard Brüel Gabrielsson <rbg@31-33-169.wireless.csail.mit.edu>
2024-05-16 13:07:50 -0400

    M

--- lolas.py
+++ lolas.py
@@ -20,6 +20,7 @@ def full_lora_pca(A, B, r, niter=10, display=True):
     V, _ = torch.linalg.qr(torch.randn(n, r))
 
     print(torch.randn(m, r))
+    assert(False)
 
     # ABt_prods = torch.mean( torch.stack([A[i] @ B[i].t() for i in range(dataset_size)]), dim=0 )
     # U, _, V = torch.svd_lowrank(ABt_prods, q=r+2, niter=2)
